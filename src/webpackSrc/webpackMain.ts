ecef34a2ddd843f560184d900e9c66a1f5502ed9
Justin Cheong <justin_cheong@tech.gov.sg>
2023-02-19 00:35:36 +0800

    update message

--- src/webpackSrc/webpackMain.ts
+++ src/webpackSrc/webpackMain.ts
@@ -1,4 +1,4 @@
 import { randomNumber } from './utility';
 
 const myDiv = document.getElementById('my-content') as HTMLDivElement;
-myDiv.textContent = `hello world with webpack and node ${randomNumber(0, 100)}`
+myDiv.textContent = `hello world with webpack and node! ${randomNumber(0, 100)}`
